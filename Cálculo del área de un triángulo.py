7fedb56d69599df2ebce679a506bc9acfc3535f4
Ulises Salazar Lopez <victor.salazarlopez@cesunbc.edu.mx>
2024-09-15 17:40:14 -0700

    Add files via upload

---
+++ Cálculo del área de un triángulo.py
@@ -0,0 +1,7 @@
+def area_triangulo():
+    print("Bienvenido calcularemos el área de un triangulo ")
+    base = float(input("Introduce la base del triángulo: "))
+    altura = float(input("Introduce la altura del triángulo: "))
+    area = (base * altura) / 2
+    print(f"El área del triángulo es: {area}")
+area_triangulo()
